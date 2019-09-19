7997abc502e8a4b38ab7f3c595823224094fc46c
Kevin Malone <zhaokevinma@gmail.com>
2019-09-19 18:19:03 -0400

    Can make purchase

--- bamazonCRUD.js
+++ bamazonCRUD.js
@@ -27,7 +27,7 @@ function start() {
             if(answer.action === "LOOK") {
                 displayProducts();
             } else {
-                connection.end();
+                buyAProduct();
             }
         });
 }
@@ -38,5 +38,58 @@ function displayProducts() {
         if(err) throw err;
         console.log(res);
         connection.end();
+    });
+}
+
+function buyAProduct() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if(err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                        var choiceArray = [];
+                        for (let i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What item would you like to Buy?"
+                },
+                {
+                    name: "amount",
+                    type: "input",
+                    message: "How many of them would you like to buy?"
+                }
+            ])
+            .then(function(answer) {
+                var chosenItem;
+                var stockQuantity;
+                for (let i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.choice) {
+                        chosenItem = res[i];
+                        stockQuantity = res[i].stock_quantity - answer.amount;
+                    
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: stockQuantity
+                                },
+                                {
+                                    item_id: chosenItem.item_id
+                                }
+                            ],
+                            function(err) {
+                                if(err) throw err;
+                                console.log("You made a purchase successfully!");
+                                start();
+                            }
+                        );
+                    }
+                }
+            });
     });
 }
